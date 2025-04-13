9c45428e03a2d46d7ae34416d5a0688478cbf9c8
andr <andr3wkm123@gmail.com>
2025-04-13 15:13:30 -0500

    misc phase 1

--- include.js
+++ include.js
@@ -15,6 +15,81 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(res => res.text())
       .then(data => {
         document.getElementById("videoModal-placeholder").innerHTML = data;
+
+        const container = document.querySelector(".video-preview"),
+        video = container.querySelector("video"),
+        background = container.querySelector("img"),
+        modal = document.querySelector(".video-modal"),
+        youtubeContainer = modal.querySelector(".video");
+    
+        container.onmouseover = () => {
+            video.classList.add("is-animated");
+            video.play();
+            setTimeout(() => {
+                background.classList.remove("is-animated");
+            }, 500)
+        }
+        
+        container.onmouseout = () => {
+            background.classList.add("is-animated");
+            setTimeout(() => {
+                video.classList.remove("is-animated");
+                video.pause();
+            }, 500)
+        }
+        
+        let iframeCreated = false;
+        let videoIframe;
+        
+        container.onclick = () => {
+            modal.classList.replace('none', 'show');
+        
+            setTimeout(() => {
+                const youtubeContainer = modal.querySelector(".video");
+                modal.classList.replace("inactive", "active");
+        
+                if (!iframeCreated) {
+                    videoIframe = document.createElement("iframe");
+                    videoIframe.src = 'https://www.youtube-nocookie.com/embed/558apeBjatI?si=IZKUqTPrItnINPmp?autoplay=1';
+                    videoIframe.title = 'YouTube video player';
+                    videoIframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+                    videoIframe.allowFullscreen = true;
+                    videoIframe.frameBorder = '0';
+                    youtubeContainer.appendChild(videoIframe);
+        
+                    iframeCreated = true;
+                }
+                else {
+                    videoIframe.src = 'https://www.youtube-nocookie.com/embed/558apeBjatI?si=IZKUqTPrItnINPmp?autoplay=1';
+                }
+            }, 100)
+        }
+
+        modal.addEventListener("click", (e) => {
+          const youtubeContainer = modal.querySelector(".video");
+          if (!youtubeContainer.contains(e.target)) {
+              closeModal();
+          }
+        });
+
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "Escape") {
+              closeModal();
+          }
+        });
+
+        function closeModal() {
+          modal.classList.replace('active', 'inactive');
+
+          if (videoIframe) {
+              videoIframe.src = 'https://www.youtube-nocookie.com/embed/558apeBjatI?si=IZKUqTPrItnINPmp?autoplay=0';
+          }
+
+          setTimeout(() => {
+              modal.classList.replace('show', 'none');
+          }, 500);
+        }
+
     });
 
     const yearSpan = document.getElementById("year");
@@ -22,5 +97,5 @@ document.addEventListener("DOMContentLoaded", () => {
         yearSpan.textContent = new Date().getFullYear();
       }
     });
-  });
+});
   
