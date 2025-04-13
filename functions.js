0c0b87e0bb6accbb596d1de4421470c147232b5a
andr <andr3wkm123@gmail.com>
2025-04-12 20:34:50 -0500

    quick website changes

--- functions.js
+++ functions.js
@@ -21,4 +21,10 @@ window.onload = function(){
     }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+});  
 
