1a75807aafc7ed95c1664caa84e2600279e7c5b9
andr <andr3wkm123@gmail.com>
2025-08-30 22:08:54 -0500

    d

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { StrictMode, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { extend } from '@react-three/fiber';
+
+import Experience from './Experience.jsx'
+import Cursor from './components/Cursor.jsx'
+import Navbar from './components/Navbar.jsx'
+import Socials from './components/Socials.jsx'
+
+import { TransitionMaterial } from "./components/TransitionMaterial";
+
+import './styles/style.css'
+
+extend({
+  TransitionMaterial,
+});
+
+export default function App() {
+  return (
+    <StrictMode>
+        <Cursor />
+        <Navbar />
+        <Socials />
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 2000,
+                position: [ 0, 0, 10 ],
+            } }
+        >
+        <Suspense fallback={null}>
+            <Experience />
+        </Suspense>
+        </Canvas>
+    </StrictMode>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,9 @@
-import { StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import {Loader} from '@react-three/drei'
-import Experience from './Experience.jsx'
-import Cursor from './components/Cursor.jsx'
-import Navbar from './components/Navbar.jsx'
-import Socials from './components/Socials.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import './styles/style.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Cursor />
-    <Navbar />
-    <Socials />
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [ 0, 0, 10 ],
-        } }
-    >
-      <Suspense fallback={null}>
-        <Experience />
-      </Suspense>
-    </Canvas>
-    <Loader />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
